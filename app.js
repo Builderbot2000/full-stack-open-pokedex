6f37fb5eb48832e16fc111f0d36b49c81c3a5876
Builderbot2000 <kevintang1118@outlook.com>
2023-08-12 20:17:20 -0700

    trigger update on git

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || LOCAL_PORT
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${LOCAL_PORT}`)
